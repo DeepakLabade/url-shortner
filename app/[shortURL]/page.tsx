1bc97c4d763fb973c64950fb8990ea7715d56f25
DeepakLabade <deepaklabade001@gmail.com>
2025-05-19 20:35:05 +0530

    added updating clicks

--- app/[shortURL]/page.tsx
+++ app/[shortURL]/page.tsx
@@ -2,12 +2,18 @@ import { redirect } from "next/navigation";
 import pgClient from "../lib/db";
 
 export default async function shortURL({params}: {params: params}) {
-    const code = params.shortURL;
+    const code = (await params).shortURL;
     const shortURL = code;
     let longURL = ""
+    let clicks = null;
+    let id = null;
     try {
-        const data = await pgClient.query("select longurl from url where shorturl = $1;", [shortURL])
+        const data = await pgClient.query("select longurl, clicks, id from url where shorturl = $1;", [shortURL])
         longURL = data.rows[0].longurl
+        clicks = data.rows[0].clicks
+        id = data.rows[0].id
+        clicks++;
+        await pgClient.query("update url set clicks = $1 where id = $2;", [clicks, id])
     } catch (error) {
         console.log("database error: " + error)
     }
